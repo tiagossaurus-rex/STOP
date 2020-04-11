264aca5b6f38176ca1f0f39cd5b534773d825362
Elucidat-Tiago <tiago@elucidat.com>
2020-04-11 12:45:49 +0100

    set up routes

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,1 +1,48 @@
-// export { Component } from './Component';
+import React from 'react';
+
+import { Switch, Route } from 'react-router-dom'
+
+const pages = [
+    {
+        exact: true,
+        id: 'home',
+        path: '/',
+        title: 'Home',
+        page: 'home',
+    },
+];
+
+const MyRoute = ({
+    title,
+    page,
+    props,
+}) => {
+
+    React.useEffect(() => {
+        document.title = title || 'T';
+    }, [])
+
+    return (
+        <Route {...props}>
+            {page}
+        </Route>
+    )
+}
+
+
+export function Routes() {
+	return (
+		<Switch>
+            { pages.map(page => {
+                if (!page.path || !page.page) return null;
+
+                return (
+                    <MyRoute
+                        key={page.id}
+                        {...page}
+                    />
+                )
+            })}
+		</Switch>
+	);
+}
